cf3bac300391be8e6274b33a1626b5dd36a71072
root <root@ip-10-0-45-226.ap-southeast-1.compute.internal>
2024-04-18 14:20:19 +0000

    Add script to perform count testing

---
+++ number_of_records.sh
@@ -0,0 +1,5 @@
+#test case to findout number of records using Unix commands between employee_initial.txt and employee_target.txt files
+
+cat /root/Desktop/EDTV/Dataset/employee_initial.txt | wc -l
+
+cat /root/Desktop/EDTV/Dataset/employee_target.txt | wc -l
